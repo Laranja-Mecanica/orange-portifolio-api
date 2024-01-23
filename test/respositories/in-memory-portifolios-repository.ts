f7851a7aa8aef89db7630f10962521a864857400
Salinas <matheussalinas3@gmail.com>
2024-01-23 14:54:04 -0300

    refactor: create portifolio use case to use entity

--- test/respositories/in-memory-portifolios-repository.ts
+++ test/respositories/in-memory-portifolios-repository.ts
@@ -1,13 +1,11 @@
-import {
-  Portifolio,
-  PortifoliosRepository,
-} from '@/domain/application/repositories/portifolios-repository'
+import { PortifoliosRepository } from '@/domain/application/repositories/portifolios-repository'
+import { Portifolio } from '@/domain/entities/portifolio'
 
 export class InMemoryPortifoliosRepository implements PortifoliosRepository {
   public items: Portifolio[] = []
 
   async findById(id: string) {
-    const portifolio = this.items.find((item) => item.id === id)
+    const portifolio = this.items.find((item) => item.id.toString() === id)
 
     if (!portifolio) {
       return null
@@ -31,7 +29,7 @@ export class InMemoryPortifoliosRepository implements PortifoliosRepository {
   }
 
   async delete(id: string) {
-    const itemIndex = this.items.findIndex((item) => item.id === id)
+    const itemIndex = this.items.findIndex((item) => item.id.toString() === id)
 
     this.items.splice(itemIndex, 1)
   }
