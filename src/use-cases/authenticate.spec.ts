4aed7c8d477b07c366af5ecfa5ba942580d41ec3
Henrique Matos <henriquemundimmatos@gmail.com>
2024-01-23 09:09:11 -0300

    feat: add authenticate use case

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,65 @@
+import { InMemoryUsersRepository } from 'test/respositories/in-memory-users-repository'
+import { AuthenticateUseCase } from './authenticate'
+import { hash } from 'bcrypt'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able to login an user', async () => {
+    const password = await hash('123456', 10)
+
+    inMemoryUsersRepository.items.push({
+      name: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@example.com',
+      password,
+    })
+
+    const { user } = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(inMemoryUsersRepository.items[0]).toEqual(user)
+  })
+
+  it('should not be able to login with wrong email', async () => {
+    const password = await hash('123456', 10)
+
+    inMemoryUsersRepository.items.push({
+      name: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@example.com',
+      password,
+    })
+    await expect(() =>
+      sut.execute({
+        email: 'wrongEmail@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should not be able to login with wrong password', async () => {
+    const password = await hash('123456', 10)
+
+    inMemoryUsersRepository.items.push({
+      name: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@example.com',
+      password,
+    })
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: 'wrongPassword',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,34 @@
+import { User, UsersRepository } from '@/repositories/users-repository'
+import { compare } from 'bcrypt'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or password are incorrect')
+    }
+
+    const isPasswordMatch = await compare(password, user.password)
+
+    if (!isPasswordMatch) {
+      throw new Error('Email or password are incorrect')
+    }
+
+    return { user }
+  }
+}
