388f4f669e0be95512a9d501cff90e0b3bc36423
Salinas <matheussalinas3@gmail.com>
2024-01-24 16:59:04 -0300

    test: setup to E2E tests
    Co-Authored-By: Henrique Matos <henriquemundimmatos@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
+import { register } from './http/controllers/register'
 
 export const app = express()
 
 app.use(express.json())
+app.post('/register', register)
---
+++ src/http/controllers/register.e2e-spec.ts
@@ -0,0 +1,44 @@
+import { env } from '@/env'
+import { Application } from 'express'
+import http, { Server as HTTPServer } from 'http'
+import request from 'supertest'
+import { getPrisma } from '../lib/prisma/prisma'
+
+let server: HTTPServer
+
+describe('Register (E2E)', () => {
+  let app: Application
+
+  beforeAll(async () => {
+    const { app: expressApp } = await import('@/app')
+    app = expressApp
+
+    server = http.createServer(app)
+    server.listen(env.PORT)
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
+  test('[POST] /register', async () => {
+    const response = await request(server).post('/register').send({
+      name: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const prisma = getPrisma()
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@example.com',
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
+})
