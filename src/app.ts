e59caa57d29323091f06ce2d5012df7ee8aa4023
Salinas <matheussalinas3@gmail.com>
2024-01-26 15:27:47 -0300

    chore: add suffix to controller files
    Co-Authored-By: Henrique Matos <henriquemundimmatos@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 
-import { createPortifolio } from './http/controllers/portifolios/create-portifolio'
-import { deletePortifolio } from './http/controllers/portifolios/delete-portifolio'
 import { editPortifolio } from './http/controllers/portifolios/edit-portifolio-controller'
 import { getPortifolioById } from './http/controllers/portifolios/get-portifolio-by-id-controller'
-import { authenticate } from './http/controllers/users/authenticate'
 import { fetchUserPortifolio } from './http/controllers/users/fetch-user-portifolios-controller'
-import { getUserProfileById } from './http/controllers/users/get-user-profile-by-id'
-import { register } from './http/controllers/users/register'
+
+import { createPortifolio } from './http/controllers/portifolios/create-portifolio-controller'
+import { deletePortifolio } from './http/controllers/portifolios/delete-portifolio-controller'
+import { authenticate } from './http/controllers/users/authenticate-controller'
+import { getUserProfileById } from './http/controllers/users/get-user-profile-by-id-controller'
+import { register } from './http/controllers/users/register-controller'
 import { authorize } from './http/midllewares/authenticate'
 
 export const app = express()
