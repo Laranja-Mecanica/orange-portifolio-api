dd716814636f620fc93abb9024b94244a85b348b
Henrique Matos <henriquemundimmatos@gmail.com>
2024-02-02 11:00:56 -0300

    fix: cors

--- src/http/cors/cors.config.ts
+++ src/http/cors/cors.config.ts
@@ -2,9 +2,11 @@ import { CorsOptions } from 'cors'
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://orange-portifolio.vercel.app',
+  'https://orange-app-2m9ib.ondigitalocean.app',
+  '',
 ]
 
 export const options: CorsOptions = {
   origin: allowedOrigins,
+  credentials: true,
 }
