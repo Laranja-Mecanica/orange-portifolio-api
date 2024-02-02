1136729841c37f8c74ba14a51e691edaa4bea2fa
Henrique Matos <henriquemundimmatos@gmail.com>
2024-02-02 10:17:31 -0300

    fix: cors options

--- src/http/cors/cors.config.ts
+++ src/http/cors/cors.config.ts
@@ -1,6 +1,9 @@
 import { CorsOptions } from 'cors'
 
-const allowedOrigins = ['http://localhost:3000', 'http://0.0.0.0:3000']
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://orange-portifolio.vercel.app',
+]
 
 export const options: CorsOptions = {
   origin: allowedOrigins,
