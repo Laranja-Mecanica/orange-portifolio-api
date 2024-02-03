71d413a3e356c239fa1f55d6cd2c229d388e8e25
Henrique Matos <henriquemundimmatos@gmail.com>
2024-02-03 11:30:38 -0300

    feat: add route login google

--- src/app.ts
+++ src/app.ts
@@ -42,9 +42,13 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }),
+)
+
+app.get(
   '/oauth2/redirect/google',
   passport.authenticate('google', {
-    scope: ['profile', 'email'],
     failureRedirect: 'https://orange-portifolio.vercel.app/',
     successRedirect: 'https://orange-portifolio.vercel.app/home',
   }),
