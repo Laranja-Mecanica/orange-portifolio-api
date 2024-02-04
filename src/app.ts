5a8a8fd40ee325e921932c370dd098b61b4d8277
Henrique Matos <henriquemundimmatos@gmail.com>
2024-02-04 17:54:02 -0300

    fix: redirect route oauth google

--- src/app.ts
+++ src/app.ts
@@ -66,7 +66,7 @@ app.use(
 app.use(authorize)
 
 app.get('/teste', (req: Request, res: Response) => {
-  res.redirect('https://orange-portifolio.vercel.app/user')
+  console.log(req.payload?.tokenPayload)
 })
 
 app.get('/portifolios/:id', getPortifolioById)
